9657ce9a82dea3ec6f32f9b2b7851ffbb8c4dbc0
KarinA <abdullaeva.karina.r@gmail.com>
2023-08-15 00:44:36 +0300

    main: Add binary search and 3 varinats of sorting

---
+++ binary_search.py
@@ -0,0 +1,25 @@
+# Given an array of integers nums which is sorted in ascending order, 
+# and an integer target, write a function to search target in nums. 
+# If target exists, then return its index. Otherwise, return -1.
+# You must write an algorithm with O(log n) runtime complexity.
+
+def binary_search(numbers: [int], target: int) -> int:
+    low = 0
+    high = len(numbers) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        guess = numbers[mid]
+        if guess == target:
+            return mid
+        if guess > target:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return -1
+
+
+test_numbers = [-1,0,3,5,9,12]
+assert binary_search(test_numbers, 9) == 4
+assert binary_search(test_numbers, -1) == 0
+assert binary_search(test_numbers, 4) == -1
---
+++ sort.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+import functools
+
+
+def timer(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start = datetime.now()
+        result = func(*args, **kwargs)
+        finish = datetime.now()
+        print(f'{func.__name__}: {finish - start}')
+        return result
+    return wrapper
+
+
+@timer
+def bubble_sort(numbers: [int]) -> [int]:
+    for i in range(len(numbers) -1):
+        for j in range(len(numbers) - i - 1):
+            if numbers[j] > numbers[j + 1]:
+                numbers[j + 1], numbers[j] = numbers[j], numbers[j + 1]
+    return numbers
+
+
+def find_smallest(numbers: [int]) -> int:
+    id_smallest = 0
+    for i in range(len(numbers)):
+        smallest = numbers[id_smallest]
+        if numbers[i] < smallest:
+            id_smallest = i
+    return id_smallest
+
+
+@timer
+def selection_sort(numbers: [int]) -> [int]:
+    sorted_numbers = []
+    for _ in range(len(numbers)):
+        smallest = find_smallest(numbers)
+        sorted_numbers.append(numbers.pop(smallest))
+    return sorted_numbers
+
+
+@timer
+def quick_sort(numbers: [int]) -> [int]:
+    def inner(numbers):
+        if len(numbers) < 2:
+            return numbers
+        point = numbers[0]
+        less = [num for num in numbers[1:] if num < point]
+        greater = [num for num in numbers[1:] if num >= point]
+        return inner(less) + [point] + inner(greater)
+    return inner(numbers)
+
+
+assert bubble_sort([1, 2, 5, 2, 3]) == [1, 2, 2, 3, 5]
+assert selection_sort([1, 2, 5, 2, 3]) == [1, 2, 2, 3, 5]
+assert quick_sort([1, 2, 5, 2, 3]) == [1, 2, 2, 3, 5]
