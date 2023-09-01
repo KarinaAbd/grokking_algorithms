715ffa04896112869b6b50473fb259bc7f6c4c46
KarinA <abdullaeva.karina.r@gmail.com>
2023-09-01 20:37:02 +0300

    main: Add new task for hash tables

--- hash_tables.py
+++ hash_tables.py
@@ -1,3 +1,12 @@
+# 1 Next Greater Element I
+
+# The next greater element of some element x in an array is the first greater element that is to the right of x
+# in the same array. You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1 is a subset of nums2.
+# For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j] and
+# determine the next greater element of nums2[j] in nums2. If there is no next greater element, then the answer for this query is -1.
+# Return an array ans of length nums1.length such that ans[i] is the next greater element as described above.
+
+
 def nextGreaterElement(nums1: [int], nums2: [int]) -> [int]:
     result = []
     mapping = {num: index for index, num in enumerate(nums2)}
@@ -18,3 +27,23 @@ def nextGreaterElement(nums1: [int], nums2: [int]) -> [int]:
 assert nextGreaterElement([4, 1, 2], [1, 3, 4, 2]) == [-1, 3, -1]
 assert nextGreaterElement([2, 4], [1, 2, 3, 4]) == [3, -1]
 assert nextGreaterElement([1,3,5,2,4], [6,5,4,3,2,1,7]) == [7, 7, 7, 7, 7]
+
+# 2 N-Repeated Element in Size 2N Array
+# You are given an integer array nums with the following properties:
+# - nums.length == 2 * n.
+# - nums contains n + 1 unique elements.
+# - Exactly one element of nums is repeated n times.
+# Return the element that is repeated n times.
+
+from collections import Counter
+
+
+def repeatedNTimes(nums: [int]) -> int:
+    c = Counter(nums)
+    res = c.most_common(1)
+    return res[0][0]
+
+
+assert repeatedNTimes([1, 2, 3, 3]) == 3
+assert repeatedNTimes([2, 1, 2, 5, 3, 2]) == 2
+assert repeatedNTimes([5,1,5,2,5,3,5,4]) == 5
